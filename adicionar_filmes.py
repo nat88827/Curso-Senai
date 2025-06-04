1e488011bff7acc72df9d11cfdfb8765565830dd
nat88827 <origanatasha5@gmail.com>
2025-06-04 15:55:06 -0300

    Add files via upload

---
+++ adicionar_filmes.py
@@ -0,0 +1,7 @@
+
+quantidades=int(input('Quantos filmes? '))
+contador=1
+while contador<=quantidades:
+    filmes=input(f"Digite o nome do filme {contador}: ")
+    contador=contador+1
+    print[filmes]
